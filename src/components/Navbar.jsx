cb2b9bb5ef661d06eb88d1c635bdaa802ac274da
pandeymynk <pandeyjiiofficial0@gmail.com>
2026-01-04 15:05:18 +0000

    My Portfolio is here

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import dayjs from "dayjs";
+import { navLinks } from "#constants";
+import { navIcons } from "../constants";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img src="/src/public/images/logo.svg" alt="" />
+          <p className="font-bold">Mayank's Portfolio</p>
+          <ul>
+            {navLinks.map(({ id, name }) => (
+              <li key={id}>
+                <p>{name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map(({id,img})=>(
+                    <li key={id}>
+                        <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                    </li>
+                ))}
+            </ul>
+            <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
